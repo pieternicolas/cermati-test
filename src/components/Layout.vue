d841dccc623dace0b8da3aec94f6c095efbdb4ce
Nicolas <nicolaspieter@yahoo.com>
2019-04-19 22:52:10 +0700

    initial commit

---
+++ src/components/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <slot name="notification"/>
+    <slot/>
+    <slot name="footer"/>
+    <slot name="newsletter"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
